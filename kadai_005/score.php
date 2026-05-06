764230da8894ac1bd31f3426787bb5871ada4f03
kotakun0510-byte <kotakun0510@gmail.com>
2026-05-06 16:13:23 +0900

    Add files via upload

---
+++ kadai_005/score.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <meta charset="UTF-8">
+  <title>課題5</title>
+
+  <body>
+    <p>
+      <?php 
+      // 合計点を計算する
+      $total = 80 + 60 + 55 + 40 + 100 + 25 + 80 + 95 + 30 + 60;
+
+      // 平均点を出して出力
+      echo $total /10;
+      ?>
+    </p>
+  </body>
+</html>
