7fc2c51bb0d1db62530228bee28229e2546a6f12
kotakun0510-byte <kotakun0510@gmail.com>
2026-05-08 10:08:22 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>PHP課題7</title>
+</head>
+
+<body>
+  <p>
+    <?php 
+    // 連想配列の作成
+    $onion_data = ['name' => 'onion', 'price' => 200, 'weight' => 160,];
+
+    // 連想配列の出力
+    print_r($onion_data);
+    ?>
+  </p>
+</body>
+</html>
